d6f96df5144f15de75468edf395bf9bd4bb7c8bb
cs-society-ashoka <cs.society@ashoka.edu.in>
2026-02-12 17:34:56 +0530

    CS Logo Fix ???

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -82,7 +82,7 @@ const Index = () => {
       >
         <div className="flex items-center gap-3 rounded-full bg-white/50 px-4 py-2 shadow-sm backdrop-blur-sm transition-all hover:bg-white/80">
           <img 
-            src={SOCIETY_DETAILS.logo} 
+            src="photos/logo.png" 
             alt={`${SOCIETY_DETAILS.name} Logo`} 
             className="h-8 w-8 rounded-full object-cover"
           />
