48cc3cf268240b010eab8515348fbafb4ebfb157
cs-society-ashoka <cs.society@ashoka.edu.in>
2026-02-12 17:02:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/walentine-website-kiah/',
   server: {
     host: "::",
     port: 8080,
